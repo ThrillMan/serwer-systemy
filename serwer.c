17180d06f11cf1333158111ab85f171e66aab32d
Wojciech Gładysz <96397435+ThrillMan@users.noreply.github.com>
2024-01-21 13:06:16 +0100

    Update serwer.c

--- serwer.c
+++ serwer.c
@@ -8,30 +8,17 @@ int main(){
     int mid = msgget(0x123, 0600 | IPC_CREAT);
     struct msgbuf
     {
-    long type;
+    int id;
     char text[1024];
-    } my_msg;
-    //strcpy(my_msg.text, "Hello world");
-    //my_msg.type =2;
-    //msgsnd(mid, &my_msg, strlen(my_msg.text)+1, 0);
-//zad 2 jego wyglad
-   /*while(1){
-    msgrcv(mid, &my_msg, 1024, -5, 0);
-    printf("odebrano id:%d\n",my_msg.type);
-    printf("wiadomosc:%s\n",my_msg.text);
-    
-   }*/
-    msgrcv(mid, &my_msg, 1024, 1, 0);
-    char ch;
-    int j = 0;
-    char przejsciowy[30];
-    strcpy(przejsciowy[30], strupr);
-    while (my_msg.text[j]) {
-        ch = my_msg.text[j];
-        putchar(toupper(ch));
-        j++;
-    }
- 
-   
-    printf("wieksze:%s",my_msg.text);
+    } my_msg;    my_msg.id =0;
+//     strcpy(my_msg.text, "Hello world");
+//     my_msg.type =2;
+//     msgsnd(mid, &my_msg, strlen(my_msg.text)+1, 0);
+    while(1){
+    msgrcv(mid, &my_msg, sizeof(my_msg), 0, 0);
+    printf("dostalem od klienta:%s\n",my_msg.text);
+    my_msg.id=my_msg.id;
+    msgsnd(mid, &my_msg, sizeof(my_msg), 0);
+   }
+
 }
