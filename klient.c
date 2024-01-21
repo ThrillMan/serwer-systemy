b0e09e3e6170ceb277470c53fe8204ef837f0c11
Wojciech Gładysz <96397435+ThrillMan@users.noreply.github.com>
2024-01-21 13:06:00 +0100

    Update klient.c

--- klient.c
+++ klient.c
@@ -3,16 +3,28 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <string.h>
-
+#include <unistd.h>
 int main(){
-    int mid = msgget(0x123, 0600 | IPC_CREAT);
+    int idOfServer = msgget(0x123, 0600 | IPC_CREAT);
+
+    int mid = msgget(ftok(".",getpid()), 0600 | IPC_CREAT);
+    printf("kluczem jest%d",mid);
     struct msgbuf
     {
-    long type;
+    int id;
     char text[1024];
     } my_msg;
-    strcpy(my_msg.text, "Hello world");
-    my_msg.type =1; 
-    msgsnd(mid, &my_msg, strlen(my_msg.text)+1, 0);
+    //strcpy(my_msg.text, "Hello world");
+    my_msg.id =getpid();
+    //msgsnd(0x123, &my_msg, sizeof(my_msg), 0);
+        while(1){
+    printf("podaj wiadomosc do serwera:\n");
+    fgets(my_msg.text,1024,stdin);
+    printf("wiad do serwera:%s\n",my_msg.text);
+    msgsnd(idOfServer, &my_msg, sizeof(my_msg), 0);
+    //msgrcv(mid, &my_msg, 1024, 0, 0);
+    //printf("wiadomosc od serwera:%s\n",my_msg.text);
+
+    }
     //msgrcv(mid, &my_msg, 1024, 5, 0);
 }
