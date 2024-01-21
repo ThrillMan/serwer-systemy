82b44a57dd774d66313ed45fefa394741b989243
Wojciech Gładysz <96397435+ThrillMan@users.noreply.github.com>
2024-01-21 13:57:25 +0100

    Update klient.c

--- klient.c
+++ klient.c
@@ -8,20 +8,21 @@ int main(){
     int idOfServer = msgget(0x123, 0600 | IPC_CREAT);
 
     int mid = msgget(ftok(".",getpid()), 0600 | IPC_CREAT);
-    printf("kluczem jest%d",mid);
+    printf("kluczem jest%d\n",mid);
     struct msgbuf
     {
     int id;
     char text[1024];
     } my_msg;
     //strcpy(my_msg.text, "Hello world");
-    my_msg.id =getpid();
+    my_msg.id =mid;
     //msgsnd(0x123, &my_msg, sizeof(my_msg), 0);
         while(1){
     printf("podaj wiadomosc do serwera:\n");
     fgets(my_msg.text,1024,stdin);
     printf("wiad do serwera:%s\n",my_msg.text);
     msgsnd(idOfServer, &my_msg, sizeof(my_msg), 0);
+    printf("koniec wiadomosci\n");
     //msgrcv(mid, &my_msg, 1024, 0, 0);
     //printf("wiadomosc od serwera:%s\n",my_msg.text);
 
