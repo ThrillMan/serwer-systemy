aaff83c4e9677d798a1e4351d3373fe393eb5350
Wojciech Gładysz <96397435+ThrillMan@users.noreply.github.com>
2024-01-21 18:58:09 +0100

    Update klient.c

--- klient.c
+++ klient.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
@@ -9,23 +10,31 @@ int main(){
 
     int mid = msgget(ftok(".",getpid()), 0600 | IPC_CREAT);
     printf("kluczem jest%d\n",mid);
-    struct msgbuf
+     struct msgbuf
     {
+    int subject;
     int id;
+    char name[20];
     char text[1024];
     } my_msg;
-    //strcpy(my_msg.text, "Hello world");
     my_msg.id =mid;
-    //msgsnd(0x123, &my_msg, sizeof(my_msg), 0);
+    //name
+    printf("Podaj imie:");
+    fgets(my_msg.name,20,stdin);
+    printf("Podaj id temat <0,4>:");
+    //scanf("%d\n",&my_msg.subject);
+    char temp[20];
+    fgets(temp,20,stdin);
+    my_msg.subject=atoi(temp);
+    printf("Wybrany subject:%d",my_msg.subject);
+    //subject
         while(1){
-    printf("podaj wiadomosc do serwera:\n");
+    printf("\npodaj wiadomosc do serwera:");
     fgets(my_msg.text,1024,stdin);
-    printf("wiad do serwera:%s\n",my_msg.text);
+    printf("\nwiad do serwera:%s\n",my_msg.text);
     msgsnd(idOfServer, &my_msg, sizeof(my_msg), 0);
     printf("koniec wiadomosci\n");
-    //msgrcv(mid, &my_msg, 1024, 0, 0);
-    //printf("wiadomosc od serwera:%s\n",my_msg.text);
-
+    msgrcv(mid, &my_msg, 1024, 0, 0);
+    printf("wiadomosc od serwera:%s\n",my_msg.text);
     }
-    //msgrcv(mid, &my_msg, 1024, 5, 0);
 }
