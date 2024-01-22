dc8b083b943fea558096ee1571f2df648e64ca4c
Wojciech Gładysz <96397435+ThrillMan@users.noreply.github.com>
2024-01-22 22:49:43 +0100

    obsluga roznych pokoi

--- klient.c
+++ klient.c
@@ -11,24 +11,35 @@ int main(){
     int mid = msgget(clientKey, 0666 | IPC_CREAT);
     printf("kluczem jest%d\n",mid);
 
+
      struct msgbuf
     {
     long mtype;
     int id;
+    int subject;
     char name[20];
     char text[1024];
     } my_msg;
     my_msg.id =clientKey;
-    my_msg.mtype =2;
+    //my_msg.mtype =2;
     //name
     printf("Podaj imie:");
     fgets(my_msg.name,20,stdin);
     printf("\n");
-
+    printf("Podaj o ktorym temacie chcesz podyskutowac?\n");
+    printf("1.Ogolny\n");
+    printf("2.Muzyka\n");
+    printf("3.Gry\n");
+    printf("4.Sport\n");
+    char temp[20];
+    fgets(temp,20,stdin);
+    printf("\n");
+    my_msg.subject=atoi(temp);
 
 if(fork()==0){
         while(1){
-            if(msgrcv(idOfServer, &my_msg, sizeof(my_msg), 1, 0)!=-1){
+            my_msg.mtype=my_msg.subject;
+            if(msgrcv(idOfServer, &my_msg, sizeof(my_msg), my_msg.subject, 0)!=-1){
                 my_msg.name[strcspn(my_msg.name,"\n")]=0;
                 if(my_msg.id!=clientKey){
                 printf("%s:%s\n",my_msg.name,my_msg.text);
@@ -45,7 +56,7 @@ else{
 
             printf("\n");
             //printf("\nwiad do serwera:%s\n",my_msg.text);
-            my_msg.mtype =2;
+            my_msg.mtype =10;
             msgsnd(idOfServer, &my_msg, sizeof(my_msg), 0);
         }
 
