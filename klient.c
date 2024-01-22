bf5ab683eddf197a88595aaa934263e119adc43e
Wojciech Gładysz <96397435+ThrillMan@users.noreply.github.com>
2024-01-22 16:06:04 +0100

    Update klient.c

--- klient.c
+++ klient.c
@@ -6,35 +6,42 @@
 #include <string.h>
 #include <unistd.h>
 int main(){
-    int idOfServer = msgget(0x123, 0600 | IPC_CREAT);
-
-    int mid = msgget(ftok(".",getpid()), 0600 | IPC_CREAT);
+    int idOfServer = msgget(0x123, 0666 | IPC_CREAT);
+    key_t clientKey = ftok(".",getpid());
+    int mid = msgget(clientKey, 0666 | IPC_CREAT);
     printf("kluczem jest%d\n",mid);
+
      struct msgbuf
     {
     int id;
     char name[20];
     char text[1024];
     } my_msg;
-    my_msg.id =mid;
+    my_msg.id =clientKey;
     //name
     printf("Podaj imie:");
     fgets(my_msg.name,20,stdin);
-    //printf("Podaj id temat <0,4>:");
-    //scanf("%d\n",&my_msg.subject);
-//     char temp[20];
-//     fgets(temp,20,stdin);
-//     my_msg.subject=atoi(temp);
-//     printf("Wybrany subject:%d",my_msg.subject);
-    //subject
-    printf("\npodaj wiadomosc do serwera:");
-    fgets(my_msg.text,1024,stdin);
-    printf("\nwiad do serwera:%s\n",my_msg.text);
-    msgsnd(idOfServer, &my_msg, sizeof(my_msg), 0);
-    printf("koniec wiadomosci\n");
-        while(1){
 
-    msgrcv(mid, &my_msg, 1024, 0, 0);
-    printf("wiadomosc od serwera:%s\n",my_msg.text);
+
+    while(1){
+            /*if(fork()==0){
+                printf("\npodaj wiadomosc do serwera:");
+                fgets(my_msg.text,1024,stdin);
+                printf("\nwiad do serwera:%s\n",my_msg.text);
+                msgsnd(idOfServer, &my_msg, sizeof(my_msg), 0);
+            }
+            else{
+                if(msgrcv(mid, &my_msg, 1024, 0, 0)!=-1){
+                printf("wiadomosc od serwera:%s\n",my_msg.text);
+            }
+            }*/
+        printf("\npodaj wiadomosc do serwera:");
+        fgets(my_msg.text,1024,stdin);
+        printf("\nwiad do serwera:%s\n",my_msg.text);
+        msgsnd(idOfServer, &my_msg, sizeof(my_msg), 0);
+        if(msgrcv(idOfServer, &my_msg, 1024, 0, 0)!=-1){
+            printf("wiadomosc od serwera:%s\n",my_msg.text);
+        }
+
     }
 }
