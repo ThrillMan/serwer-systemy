a58da8e4c9ab28667bf1e025cc206e4cd5c43065
Wojciech Gładysz <96397435+ThrillMan@users.noreply.github.com>
2024-01-22 18:23:06 +0100

    Update klient.c

--- klient.c
+++ klient.c
@@ -13,35 +13,47 @@ int main(){
 
      struct msgbuf
     {
+    long mtype;
     int id;
     char name[20];
     char text[1024];
     } my_msg;
     my_msg.id =clientKey;
+    my_msg.mtype =2;
     //name
     printf("Podaj imie:");
     fgets(my_msg.name,20,stdin);
 
 
-    while(1){
-            /*if(fork()==0){
-                printf("\npodaj wiadomosc do serwera:");
-                fgets(my_msg.text,1024,stdin);
-                printf("\nwiad do serwera:%s\n",my_msg.text);
-                msgsnd(idOfServer, &my_msg, sizeof(my_msg), 0);
-            }
-            else{
-                if(msgrcv(mid, &my_msg, 1024, 0, 0)!=-1){
+
+if(fork()==0){
+        while(1){
+            if(msgrcv(idOfServer, &my_msg, 1024, 1, 0)!=-1){
                 printf("wiadomosc od serwera:%s\n",my_msg.text);
             }
-            }*/
-        printf("\npodaj wiadomosc do serwera:");
-        fgets(my_msg.text,1024,stdin);
-        printf("\nwiad do serwera:%s\n",my_msg.text);
-        msgsnd(idOfServer, &my_msg, sizeof(my_msg), 0);
-        if(msgrcv(idOfServer, &my_msg, 1024, 0, 0)!=-1){
-            printf("wiadomosc od serwera:%s\n",my_msg.text);
+        }
+    }
+else{
+        while(1){
+            printf("\npodaj wiadomosc do serwera:");
+            fgets(my_msg.text,1024,stdin);
+            printf("\nwiad do serwera:%s\n",my_msg.text);
+            my_msg.mtype =2;
+            msgsnd(idOfServer, &my_msg, sizeof(my_msg), 0);
         }
 
-    }
+        }
+
+//         printf("\npodaj wiadomosc do serwera:");
+//         fgets(my_msg.text,1024,stdin);
+//         printf("\nwiad do serwera:%s\n",my_msg.text);
+//         msgsnd(idOfServer, &my_msg, sizeof(my_msg), 0);
+//         my_msg.mtype =1;
+//         if(msgrcv(idOfServer, &my_msg, 1024, 1, 0)!=-1){
+//             printf("wiadomosc od serwera:%s\n",my_msg.text);
+//         }
+
+
+
+
 }
